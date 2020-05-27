90f2e73f94c6b5e99bc772f0984b4fae39ddc9fe
Theodore Keloglou <zf@sirodoht.com>
2020-05-27 23:23:35 +0100

    add post model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,4 @@
+import markdown
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
@@ -12,3 +13,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    slug = models.CharField(max_length=300)
+    body = models.TextField(blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def as_markdown(self):
+        return markdown.markdown(
+            self.body,
+            extensions=[
+                "markdown.extensions.fenced_code",
+                "markdown.extensions.tables",
+            ],
+        )
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return self.title
