74522c30e133e9293d490c1041cd7af62e470fd5
Theodore Keloglou <zf@sirodoht.com>
2020-05-27 23:24:11 +0100

    add new post and go to blog nav links

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,15 @@ from main import views
 
 admin.site.site_header = "mataroa administration"
 
+# indexes
 urlpatterns = [
     path("", views.index, name="index"),
+    path("root/", views.root_index, name="root_index"),
+    path("blog/<slug:username>", views.blog_index, name="blog_index"),
+]
+
+# user system
+urlpatterns += [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/create/", views.UserCreate.as_view(), name="user_create"),
     path("accounts/<int:pk>/", views.UserDetail.as_view(), name="user_detail"),
