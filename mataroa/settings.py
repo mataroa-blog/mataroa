7032b4e04e6858c7114f8cedb683ef3c2c207fb4
Theodore Keloglou <zf@sirodoht.com>
2020-05-30 22:09:29 +0100

    add sentry integration

--- mataroa/settings.py
+++ mataroa/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 import dj_database_url
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -152,3 +154,10 @@ EMAIL_SUBJECT_PREFIX = "[Mataroa Notification] "
 #     SESSION_COOKIE_SECURE = True
 #     CSRF_COOKIE_SECURE = True
 #     SECURE_SSL_REDIRECT = True
+
+
+# Sentry
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"), integrations=[DjangoIntegration()],
+)
