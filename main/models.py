f1ec83901df73c44935a48899560055256bfcec0
Theodore Keloglou <zf@sirodoht.com>
2020-05-30 01:47:32 +0100

    change to cascade posts remove on user delete

--- main/models.py
+++ main/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     title = models.CharField(max_length=300)
     slug = models.CharField(max_length=300)
     body = models.TextField(blank=True, null=True)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
