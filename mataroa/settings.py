68b4d747566d3c88469dc51a311fb7d288416765
Theodore Keloglou <zf@sirodoht.com>
2020-05-30 23:12:50 +0100

    center everything

--- mataroa/settings.py
+++ mataroa/settings.py
@@ -29,7 +29,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "nonrandom_secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get("NODEBUG") is None else False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".mataroa.blog", ".mataroalocal.blog", "95.217.176.64"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".mataroa.blog",
+    ".mataroalocal.blog",
+    "95.217.176.64",
+]
 
 ADMINS = [("Wang Nui", "admin@mataroa.blog")]
 
