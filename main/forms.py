c2e69cfad0592c86f65468a5c48bbdd37c62556f
Theodore Keloglou <zf@sirodoht.com>
2020-05-30 17:37:44 +0100

    add interest form

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,17 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm as DjUserCreationForm
+from django.core.mail import mail_admins
 
 
 class UserCreationForm(DjUserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ["username"]
+
+
+class InterestForm(forms.Form):
+    email = forms.EmailField()
+
+    def send_email(self):
+        mail_admins("Interest form response", self.cleaned_data.get("email"))
