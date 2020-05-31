7207357d5caedeb2be3f64513c66e4b93144d5c9
Theodore Keloglou <zf@sirodoht.com>
2020-05-31 10:44:47 +0100

    change admin email

--- mataroa/settings.py
+++ mataroa/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = [
     "95.217.176.64",
 ]
 
-ADMINS = [("Wang Nui", "admin@mataroa.blog")]
+ADMINS = [("Theodore Keloglou", "zf@sirodoht.com")]
 
 # leading slashes are important so that browser do not handle it as internal url
 CANONICAL_HOST = "mataroa.blog"
