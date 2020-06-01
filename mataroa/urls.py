78bbd77b876033de662696ae11c2788f2b7f0bbc
Theodore Keloglou <zf@sirodoht.com>
2020-06-01 22:26:44 +0100

    change admin url

--- mataroa/urls.py
+++ mataroa/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("whale/", admin.site.urls),
     path("", include("main.urls")),
 ]
