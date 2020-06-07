a7370f13db2093d1059d7c6bbe5421fa422e2d93
Theodore Keloglou <zf@sirodoht.com>
2020-06-07 02:04:18 +0100

    add domain name validator

--- main/validators.py
+++ main/validators.py
@@ -1,9 +1,13 @@
 from django.core import validators
-from django.utils.deconstruct import deconstructible
+from django.core.exceptions import ValidationError
 
 
-@deconstructible
 class StrictUsernameValidator(validators.RegexValidator):
     regex = r"^[a-z\d-]+\Z"
     message = "Enter a valid username, it should include only lowercase letters, numbers, and -"
     flags = 0
+
+
+def validate_domain_name(value):
+    if "." not in value:
+        raise ValidationError("Invalid domain name")
