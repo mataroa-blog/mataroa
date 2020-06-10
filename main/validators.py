acdfad2adcad46bcddfadd2412987eab6e4beb7a
Theodore Keloglou <zf@sirodoht.com>
2020-06-10 01:16:10 +0100

    change django validator import to be consistent

--- main/validators.py
+++ main/validators.py
@@ -1,8 +1,8 @@
-from django.core import validators
+from django.core import validators as dj_validators
 from django.core.exceptions import ValidationError
 
 
-class StrictUsernameValidator(validators.RegexValidator):
+class StrictUsernameValidator(dj_validators.RegexValidator):
     regex = r"^[a-z\d-]+\Z"
     message = "Enter a valid username, it should include only lowercase letters, numbers, and -"
     flags = 0
