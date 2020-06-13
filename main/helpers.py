4b86a12f032fd8ebbbaed8ab31ac8dbbbe531936
Theodore Keloglou <zf@sirodoht.com>
2020-06-13 01:35:37 +0100

    add pygments highlight helper function

--- main/helpers.py
+++ main/helpers.py
@@ -1,6 +1,9 @@
 import uuid
 
+import pygments
 from django.utils.text import slugify
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_for_filename
 
 from main import models
 
@@ -66,3 +69,33 @@ def get_post_slug(post_title, owner):
     if models.Post.objects.filter(owner=owner, slug=slug).exists():
         slug += "-" + str(uuid.uuid4())[:8]
     return slug
+
+
+def syntax_highlight(text):
+    """Highlights markdown codeblocks within a markdown text."""
+    highlighted_text = ""
+    within_codeblock = False
+    lexer = None
+    code_block = ""
+    for line in text.split("\n"):
+        if within_codeblock and lexer:
+            if line[:3] == "```":
+                # end of code block, highlight it now
+                within_codeblock = False
+                highlighted_codeblock = pygments.highlight(
+                    code_block, lexer, HtmlFormatter(noclasses=True, cssclass="")
+                )
+                highlighted_text += highlighted_codeblock
+                continue
+            else:
+                code_block += line
+        else:
+            if line[:3] != "```":
+                highlighted_text += line
+
+        if line[:3] == "```" and not within_codeblock:
+            lang_ext = "file." + line[3:].strip()
+            lexer = get_lexer_for_filename(lang_ext)
+            within_codeblock = True
+
+    return highlighted_text
