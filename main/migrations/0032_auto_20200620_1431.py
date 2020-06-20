e2b682a1f2e82e02f04bd48676aab41f522838f5
Theodore Keloglou <zf@sirodoht.com>
2020-06-20 16:47:07 +0100

    move webring to all blog pages

--- main/migrations/0032_auto_20200620_1431.py
+++ main/migrations/0032_auto_20200620_1431.py
@@ -6,18 +6,28 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('main', '0031_auto_20200620_1344'),
+        ("main", "0031_auto_20200620_1344"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='user',
-            name='webring_next_url',
-            field=models.URLField(blank=True, help_text="URL for your webring's next website.", null=True, verbose_name='Webring next URL'),
+            model_name="user",
+            name="webring_next_url",
+            field=models.URLField(
+                blank=True,
+                help_text="URL for your webring's next website.",
+                null=True,
+                verbose_name="Webring next URL",
+            ),
         ),
         migrations.AlterField(
-            model_name='user',
-            name='webring_prev_url',
-            field=models.URLField(blank=True, help_text="URL for your webring's previous website.", null=True, verbose_name='Webring previous URL'),
+            model_name="user",
+            name="webring_prev_url",
+            field=models.URLField(
+                blank=True,
+                help_text="URL for your webring's previous website.",
+                null=True,
+                verbose_name="Webring previous URL",
+            ),
         ),
     ]
