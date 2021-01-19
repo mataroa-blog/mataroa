3261e2e8ff2bfa40f50b907e4d20caac5edec8be
Theodore Keloglou <zf@sirodoht.com>
2021-01-19 17:54:57 +0000

    add comment on manage.py

--- manage.py
+++ manage.py
@@ -7,8 +7,8 @@ import dotenv
 
 
 def main():
+    """Run administrative tasks."""
     dotenv.read_dotenv()
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mataroa.settings")
     try:
         from django.core.management import execute_from_command_line
