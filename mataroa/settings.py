2cd215968aae4f1b84e6595ef73c1e6d3d12b791
Theodore Keloglou <zf@sirodoht.com>
2021-01-24 19:52:54 +0000

    add email host for newsletter

--- mataroa/settings.py
+++ mataroa/settings.py
@@ -145,6 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "_static")
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.postmarkapp.com"
+EMAIL_HOST_BROADCASTS = "smtp-broadcasts.postmarkapp.com"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
