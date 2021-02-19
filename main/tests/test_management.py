e595b823c2bd2dac0f3324bcac36948a1b6eaa10
Theodore Keloglou <zf@sirodoht.com>
2021-02-19 19:18:34 +0000

    cleanup management tests

--- main/tests/test_management.py
+++ main/tests/test_management.py
@@ -40,7 +40,7 @@ class EnqueueNotificationsTest(TestCase):
         models.Post.objects.create(owner=self.user, **post_data)
 
         # as inactive, it should be ignored by the enqueue functionality
-        self.notification_inactive = models.Notification.objects.create(
+        models.Notification.objects.create(
             blog_user=self.user,
             email="inactive@example.com",
             is_active=False,
@@ -85,108 +85,6 @@ class ProcessNotificationsTest(TestCase):
     """
     Test process_notifications sends emails to the subscibers of the
     NotificationRecords that exist.
-    """
-
-    def setUp(self):
-        self.user = models.User.objects.create(
-            username="alice", email="alice@wonderland.com", notifications_on=True
-        )
-
-        post_data = {
-            "title": "Yesterday post",
-            "slug": "yesterday-post",
-            "body": "Content sentence.",
-            "published_at": timezone.make_aware(datetime(2020, 1, 1)),
-        }
-        self.post_yesterday = models.Post.objects.create(owner=self.user, **post_data)
-
-        post_data = {
-            "title": "Today post",
-            "slug": "today-post",
-            "body": "Content sentence.",
-            "published_at": timezone.make_aware(datetime(2020, 1, 2)),
-        }
-        self.post_today = models.Post.objects.create(owner=self.user, **post_data)
-
-        self.notification = models.Notification.objects.create(
-            blog_user=self.user, email="zf@sirodoht.com"
-        )
-        self.notificationrecord = models.NotificationRecord.objects.create(
-            notification=self.notification,
-            post=self.post_yesterday,
-            sent_at=None,
-        )
-
-    def test_mail_backend(self):
-        connection = process_notifications.get_mail_connection()
-        self.assertEqual(connection.host, settings.EMAIL_HOST_BROADCASTS)
-
-    def test_command(self):
-        output = StringIO()
-
-        with patch.object(
-            timezone, "now", return_value=datetime(2020, 1, 2, 13, 00)
-        ), patch.object(
-            # Django default test runner overrides SMTP EmailBackend with locmem,
-            # but because we re-import the SMTP backend in
-            # process_notifications.get_mail_connection, we need to mock it here too.
-            process_notifications,
-            "get_mail_connection",
-            return_value=mail.get_connection(
-                "django.core.mail.backends.locmem.EmailBackend"
-            ),
-        ):
-            call_command("process_notifications", stdout=output)
-
-        # notification record
-        self.assertEqual(len(models.NotificationRecord.objects.all()), 1)
-        self.assertEqual(
-            models.NotificationRecord.objects.first().notification.email,
-            self.notification.email,
-        )
-        self.assertEqual(
-            models.NotificationRecord.objects.first().post.title, "Yesterday post"
-        )
-        self.assertIsNotNone(models.NotificationRecord.objects.first().sent_at.date())
-
-        # logging
-        self.assertIn("Processing notifications.", output.getvalue())
-        self.assertIn("Broadcast sent. Total 1 emails.", output.getvalue())
-        self.assertNotIn("Today post", output.getvalue())
-
-        # email
-        self.assertEqual(len(mail.outbox), 1)
-        self.assertEqual(
-            mail.outbox[0].subject, "alice new post publication: Yesterday post"
-        )
-        self.assertIn("To unsubscribe", mail.outbox[0].body)
-
-        # email headers
-        self.assertEqual(mail.outbox[0].to, [self.notification.email])
-        self.assertEqual(mail.outbox[0].reply_to, [self.user.email])
-        self.assertEqual(mail.outbox[0].from_email, settings.NOTIFICATIONS_FROM_EMAIL)
-
-        self.assertEqual(
-            mail.outbox[0].extra_headers["X-PM-Message-Stream"], "newsletters"
-        )
-        self.assertIn(
-            "/newsletter/unsubscribe/",
-            mail.outbox[0].extra_headers["List-Unsubscribe"],
-        )
-        self.assertEqual(
-            mail.outbox[0].extra_headers["List-Unsubscribe-Post"],
-            "List-Unsubscribe=One-Click",
-        )
-
-    def tearDown(self):
-        models.User.objects.all().delete()
-        models.Post.objects.all().delete()
-
-
-class NonActiveNotificationsTest(TestCase):
-    """
-    Test process_notifications does not send emails to is_active=False
-    subscibers.
     """
 
     def setUp(self):
