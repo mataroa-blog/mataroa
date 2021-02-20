27b40d6c846ff23af17a07bc18cf5258d8dae6cc
Theodore Keloglou <zf@sirodoht.com>
2021-02-20 12:41:59 +0000

    change notification processing to 3pm

--- main/management/commands/process_notifications.py
+++ main/management/commands/process_notifications.py
@@ -65,7 +65,7 @@ class Command(BaseCommand):
     help = "Processes new posts and sends email to subscribers"
 
     def handle(self, *args, **options):
-        if timezone.now().hour != 13:
+        if timezone.now().hour != 15:
             self.stdout.write(self.style.NOTICE("No action. Current UTC is not 13:00."))
             return
 
