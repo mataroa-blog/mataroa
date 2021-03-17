b89f2431b02799bd33149d4ffc8c06a56602e22c
Theodore Keloglou <zf@sirodoht.com>
2021-03-17 15:37:52 +0000

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/local/bin/bash
+
+set -e
+set -x
+
+# pull latest changes
+ssh roa@mataroa.com 'cd mataroa && git pull'
+
+# sync requirements
+ssh roa@mataroa.blog 'cd mataroa && source venv/bin/activate && pip install -r requirements.txt'
+
+# collect static
+ssh roa@mataroa.blog 'cd mataroa && source venv/bin/activate && python manage.py collectstatic --noinput'
+
+# migrate database
+ssh roa@mataroa.blog 'cd mataroa && source venv/bin/activate && DATABASE_URL=$DATABASE_URL python3 manage.py migrate'
+
+# touch uwsgi ini to trigger reload
+ssh roa@mataroa.blog 'cd mataroa && source venv/bin/activate && uwsgi --reload mataroa.pid'
