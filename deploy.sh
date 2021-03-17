f210f967dc404618002be50a122afd9c91ae46c1
Theodore Keloglou <zf@sirodoht.com>
2021-03-17 16:18:34 +0000

    fix deploy script env

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,18 @@
 #!/usr/local/bin/bash
 
 set -e
-set -x
 
 # pull latest changes
-ssh roa@mataroa.blog 'cd mataroa && git pull'
+ssh roa@95.217.177.163 'cd mataroa && git pull'
 
 # sync requirements
-ssh roa@mataroa.blog 'cd mataroa && source venv/bin/activate && pip install -r requirements.txt'
+ssh roa@95.217.177.163 'cd mataroa && source venv/bin/activate && pip install -r requirements.txt'
 
 # collect static
-ssh roa@mataroa.blog 'cd mataroa && source venv/bin/activate && python manage.py collectstatic --noinput'
+ssh roa@95.217.177.163 'cd mataroa && source venv/bin/activate && python manage.py collectstatic --noinput'
 
 # migrate database
-ssh roa@mataroa.blog 'cd mataroa && source venv/bin/activate && DATABASE_URL=$DATABASE_URL python manage.py migrate'
+env $(cat .env.prod | xargs) ssh roa@95.217.177.163 'cd mataroa && source venv/bin/activate && python manage.py migrate'
 
-# touch uwsgi ini to trigger reload
-ssh roa@mataroa.blog 'cd mataroa && source venv/bin/activate && uwsgi --reload mataroa.pid'
+# reload
+ssh roa@95.217.177.163 'cd mataroa && source venv/bin/activate && uwsgi --reload mataroa.pid'
