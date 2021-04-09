2604e18cb2c5e3c800b484fc57c083f80dbbe696
Theodore Keloglou <zf@sirodoht.com>
2021-04-09 14:24:17 +0100

    migrate all models primary keys to BigAutoField

--- mataroa/settings.py
+++ mataroa/settings.py
@@ -150,6 +150,12 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
 # Email
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
