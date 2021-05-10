06ea7af44ba60707c704b5aec6c76e5a46e597b7
Theodore Keloglou <zf@sirodoht.com>
2021-05-11 00:49:35 +0100

    split disallowed usernames et al into separate module

--- main/util.py
+++ main/util.py
@@ -8,101 +8,48 @@ from django.utils.text import slugify
 from pygments.formatters import HtmlFormatter
 from pygments.lexers import ClassNotFound, get_lexer_by_name, get_lexer_for_filename
 
-from main import models
+from main import denylist, models
 
 
 def is_disallowed(username):
+    """Return true if username is not allowed to be registered."""
     if username[0] == "_":
         # do not allow leading underscores
         return True
-
-    disallowed_usernames = [
-        "about",
-        "abuse",
-        "account",
-        "accounts",
-        "admin",
-        "administration",
-        "administrator",
-        "api",
-        "auth",
-        "authentication",
-        "billing",
-        "blog",
-        "blogs",
-        "cdn",
-        "collection",
-        "config",
-        "contact",
-        "dash",
-        "dashboard",
-        "developer",
-        "developers",
-        "docs",
-        "documentation",
-        "help",
-        "helpcenter",
-        "home",
-        "image",
-        "images",
-        "img",
-        "index",
-        "knowledgebase",
-        "legal",
-        "login",
-        "logout",
-        "man",
-        "manual",
-        "meta",
-        "metrics",
-        "on",
-        "ops",
-        "pricing",
-        "privacy",
-        "profile",
-        "random",
-        "register",
-        "registration",
-        "search",
-        "settings",
-        "setup",
-        "signin",
-        "signup",
-        "smtp",
-        "static",
-        "status",
-        "support",
-        "terms",
-        "test",
-        "tests",
-        "tmp",
-        "wiki",
-        "www",
-    ]
-    return username in disallowed_usernames
+    return username in denylist.DISALLOWED_USERNAMES
 
 
 def get_post_slug(post_title, owner):
+    """Generate slug given post title."""
     slug = slugify(post_title)
-    if not slug:  # in case of post_title such as این متن است
+
+    # in case of post_title such as این متن است
+    if not slug:
         slug = str(uuid.uuid4())[:8]
+
+    # if slug exists, add a unique string suffix
     if models.Post.objects.filter(owner=owner, slug=slug).exists():
         slug += "-" + str(uuid.uuid4())[:8]
+
     return slug
 
 
 def syntax_highlight(text):
     """Highlights markdown codeblocks within a markdown text."""
+
     processed_text = ""
     within_code_block = False
     lexer = None
     code_block = ""
     for line in text.split("\n"):
+
+        # code block backticks found, either begin or end
         if line[:3] == "```":
-            # code block backticks found, either begin or end
+
             if not within_code_block:
-                # this is the beginning of a block
+                # then this is the beginning of a block
                 lang = line[3:].strip()
+
                 if lang:
                     # then this is a *code* block
                     within_code_block = True
@@ -121,6 +68,7 @@ def syntax_highlight(text):
                 else:
                     # no lang, so just a generic block (non-code)
                     lexer = None
+
             else:
                 # then this is the end of a code block
                 # actual highlighting happens here
@@ -150,85 +98,16 @@ def clean_html(dirty_html, strip_tags=False):
     if strip_tags:
         return bleach.clean(dirty_html, strip=True)
 
-    elements = [
-        "a",
-        "abbr",
-        "acronym",
-        "article",
-        "b",
-        "bdi",
-        "bdo",
-        "br",
-        "blockquote",
-        "cite",
-        "code",
-        "div",
-        "dfn",
-        "em",
-        "kbd",
-        "h1",
-        "h2",
-        "h3",
-        "h4",
-        "h5",
-        "h6",
-        "hr",
-        "i",
-        "iframe",
-        "img",
-        "li",
-        "mark",
-        "ol",
-        "p",
-        "pre",
-        "q",
-        "rb",
-        "rt",
-        "rtc",
-        "ruby",
-        "s",
-        "samp",
-        "section",
-        "small",
-        "span",
-        "strong",
-        "sub",
-        "sup",
-        "time",
-        "u",
-        "ul",
-        "var",
-        "wbr",
-    ]
-
-    attrs = [
-        "alt",
-        "class",
-        "height",
-        "href",
-        "id",
-        "seamless",
-        "src",
-        "style",
-        "title",
-        "width",
-        "frameborder",
-    ]
-
-    styles = [
-        "background",
-        "border",
-        "border-radius",
-        "color",
-        "display",
-        "height",
-        "width",
-    ]
-
-    return bleach.clean(dirty_html, tags=elements, attributes=attrs, styles=styles)
+    return bleach.clean(
+        dirty_html,
+        tags=denylist.ALLOWED_HTML_ELEMENTS,
+        attributes=denylist.ALLOWED_HTML_ATTRS,
+        styles=denylist.ALLOWED_CSS_STYLES,
+    )
 
 
 def md_to_html(markdown_string, strip_tags=False):
+    """Return HTML formatted string, given a markdown one."""
     dirty_html = markdown.markdown(
         syntax_highlight(markdown_string),
         extensions=[
