2118edff4cb4f5dffb9fd95a4d7d92f35d1778ab
Theodore Keloglou <zf@sirodoht.com>
2021-06-27 17:50:02 +0100

    add test cases for static pages

---
+++ main/tests/test_static.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from main import models
+
+
+class StaticTestCase(TestCase):
+    def test_modus(self):
+        response = self.client.get(reverse("modus"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_privacy(self):
+        response = self.client.get(reverse("privacy"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_guides_markdown(self):
+        response = self.client.get(reverse("guides_markdown"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_guides_images(self):
+        response = self.client.get(reverse("guides_images"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_comparisons(self):
+        response = self.client.get(reverse("comparisons"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_export(self):
+        """Test export index page as an anon user."""
+        response = self.client.get(reverse("export_index"))
+        self.assertEqual(response.status_code, 200)
