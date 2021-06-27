0d155d3bb9b0a8c7350a01eab38abc6856a90422
Theodore Keloglou <zf@sirodoht.com>
2021-06-27 17:32:16 +0100

    add blog export epub test case

--- main/tests/test_blog.py
+++ main/tests/test_blog.py
@@ -214,6 +214,26 @@ class BlogExportHugoTestCase(TestCase):
         self.assertContains(response, self.data["slug"].encode("utf-8"))
 
 
+class BlogExportEpubTestCase(TestCase):
+    def setUp(self):
+        self.user = models.User.objects.create(username="alice")
+        self.client.force_login(self.user)
+        self.data = {
+            "title": "Welcome post",
+            "slug": "welcome-post",
+            "body": "Content sentence.",
+        }
+        self.post = models.Post.objects.create(owner=self.user, **self.data)
+
+    def test_blog_export(self):
+        response = self.client.post(reverse("export_epub"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response["Content-Type"], "application/epub")
+        self.assertContains(response, "OEBPS/titlepage.xhtml".encode("utf-8"))
+        self.assertContains(response, "OEBPS/toc.xhtml".encode("utf-8"))
+        self.assertContains(response, "OEBPS/author.xhtml".encode("utf-8"))
+
+
 class RSSFeedTestCase(TestCase):
     def setUp(self):
         self.user = models.User.objects.create(username="alice")
