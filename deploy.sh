a5aa06955dce9c59f55a40eb4f12e494fa8623c7
Theodore Keloglou <zf@sirodoht.com>
2021-07-31 13:54:40 +0300

    consolidate deploy commands

--- deploy.sh
+++ deploy.sh
@@ -11,17 +11,11 @@ source venv/bin/activate
 python manage.py test
 deactivate
 
-# pull changes on server
-ssh roa@95.217.177.163 'cd mataroa && git pull'
-
-# sync requirements
-ssh roa@95.217.177.163 'cd mataroa && source venv/bin/activate && pip install -r requirements.txt'
-
-# collect static
-ssh roa@95.217.177.163 'cd mataroa && source venv/bin/activate && python manage.py collectstatic --noinput'
-
-# migrate database
-ssh roa@95.217.177.163 'cd mataroa && source venv/bin/activate && source .envrc && python manage.py migrate'
-
-# reload
-ssh roa@95.217.177.163 'cd mataroa && source venv/bin/activate && uwsgi --reload mataroa.pid'
+# pull and reload on server
+ssh roa@95.217.177.163 'cd mataroa \
+    && git pull \
+    && source venv/bin/activate \
+    && pip install -r requirements.txt \
+    && python manage.py collectstatic --noinput \
+    && python manage.py migrate \
+    && uwsgi --reload mataroa.pid'
