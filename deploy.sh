34988e0f0e16c8da96e4955f8e6f42ad8627e409
Theodore Keloglou <zf@sirodoht.com>
2021-08-21 23:05:23 +0300

    update deploy script formatting

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,11 @@ source venv/bin/activate
 python manage.py test
 deactivate
 
-# pull and reload on server
+# pull on server and reload
 ssh roa@95.217.177.163 'cd mataroa \
     && git pull \
     && source venv/bin/activate \
     && pip install -r requirements.txt \
     && python manage.py collectstatic --noinput \
-    && source .envrc \
-    && python manage.py migrate \
+    && source .envrc && python manage.py migrate \
     && uwsgi --reload mataroa.pid'
