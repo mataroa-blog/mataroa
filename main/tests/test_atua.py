1f1fbe9cd3a3e0e7601f64a7ebe7e03cbc12875e
Theodore Keloglou <zf@sirodoht.com>
2022-05-25 01:24:30 +0100

    add tests for atua comments

--- main/tests/test_atua.py
+++ main/tests/test_atua.py
@@ -81,3 +81,162 @@ class AtuaAdminTestCase(TestCase):
     def test_atua_comments_admin(self):
         response = self.client.get(reverse("atua_comments"))
         self.assertEqual(response.status_code, 200)
+
+
+class AtuaCommentsTestCase(TestCase):
+    def setUp(self):
+        self.admin = models.User.objects.create(username="alice", is_superuser=True)
+        self.user = models.User.objects.create(username="bob", comments_on=True)
+        self.post = models.Post.objects.create(
+            owner=self.user,
+            title="Welcome post",
+            slug="welcome-post",
+            body="Content sentence.",
+            published_at="2020-02-06",
+        )
+        self.client.force_login(self.user)
+
+    def test_comment_list(self):
+        data = {
+            "name": "Jon",
+            "email": "jon@wick.com",
+            "body": "Comment here.",
+        }
+        response = self.client.post(
+            reverse("comment_create", args=(self.post.slug,)),
+            HTTP_HOST=f"{self.user.username}.{settings.CANONICAL_HOST}",
+            data=data,
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(models.Comment.objects.all().count(), 1)
+        self.assertEqual(models.Comment.objects.all().first().name, data["name"])
+        self.assertEqual(models.Comment.objects.all().first().email, data["email"])
+        self.assertEqual(models.Comment.objects.all().first().body, data["body"])
+        self.assertEqual(models.Comment.objects.all().first().post, self.post)
+        self.assertEqual(models.Comment.objects.all().first().is_approved, False)
+
+
+class AtuaCommentsApproveNoAdminTestCase(TestCase):
+    def setUp(self):
+        self.admin = models.User.objects.create(username="alice", is_superuser=True)
+        self.user = models.User.objects.create(username="bob", comments_on=True)
+        self.post = models.Post.objects.create(
+            owner=self.user,
+            title="Welcome post",
+            slug="welcome-post",
+            body="Content sentence.",
+            published_at="2020-02-06",
+        )
+        self.comment = models.Comment.objects.create(
+            post=self.post,
+            name="Jon",
+            email="jon@wick.com",
+            body="Comment here.",
+            is_approved=False,
+        )
+        self.client.force_login(self.user)
+
+    def test_comment_approve_not_authorised(self):
+        data = {
+            "is_approved": True,
+        }
+        response = self.client.post(
+            reverse("atua_comment_approve", args=(self.comment.id,)),
+            HTTP_HOST=f"{self.user.username}.{settings.CANONICAL_HOST}",
+            data=data,
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(models.Comment.objects.all().count(), 1)
+        self.assertEqual(models.Comment.objects.all().first().is_approved, False)
+
+
+class AtuaCommentsApproveTestCase(TestCase):
+    def setUp(self):
+        self.admin = models.User.objects.create(username="alice", is_superuser=True)
+        self.user = models.User.objects.create(username="bob", comments_on=True)
+        self.post = models.Post.objects.create(
+            owner=self.user,
+            title="Welcome post",
+            slug="welcome-post",
+            body="Content sentence.",
+            published_at="2020-02-06",
+        )
+        self.comment = models.Comment.objects.create(
+            post=self.post,
+            name="Jon",
+            email="jon@wick.com",
+            body="Comment here.",
+            is_approved=False,
+        )
+        self.client.force_login(self.admin)
+
+    def test_comment_approve(self):
+        data = {
+            "is_approved": True,
+        }
+        response = self.client.post(
+            reverse("atua_comment_approve", args=(self.comment.id,)),
+            HTTP_HOST=f"{self.user.username}.{settings.CANONICAL_HOST}",
+            data=data,
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(models.Comment.objects.all().count(), 1)
+        self.assertEqual(models.Comment.objects.all().first().is_approved, True)
+
+
+class AtuaCommentsDeleteNoAuthTestCase(TestCase):
+    def setUp(self):
+        self.admin = models.User.objects.create(username="alice", is_superuser=True)
+        self.user = models.User.objects.create(username="bob", comments_on=True)
+        self.post = models.Post.objects.create(
+            owner=self.user,
+            title="Welcome post",
+            slug="welcome-post",
+            body="Content sentence.",
+            published_at="2020-02-06",
+        )
+        self.comment = models.Comment.objects.create(
+            post=self.post,
+            name="Jon",
+            email="jon@wick.com",
+            body="Comment here.",
+            is_approved=False,
+        )
+        self.client.force_login(self.user)
+
+    def test_comment_approve_not_authorised(self):
+        response = self.client.post(
+            reverse("atua_comment_delete", args=(self.comment.id,)),
+            HTTP_HOST=f"{self.user.username}.{settings.CANONICAL_HOST}",
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(models.Comment.objects.all().count(), 1)
+
+
+class AtuaCommentsDeleteTestCase(TestCase):
+    def setUp(self):
+        self.admin = models.User.objects.create(username="alice", is_superuser=True)
+        self.user = models.User.objects.create(username="bob", comments_on=True)
+        self.post = models.Post.objects.create(
+            owner=self.user,
+            title="Welcome post",
+            slug="welcome-post",
+            body="Content sentence.",
+            published_at="2020-02-06",
+        )
+        self.comment = models.Comment.objects.create(
+            post=self.post,
+            name="Jon",
+            email="jon@wick.com",
+            body="Comment here.",
+            is_approved=False,
+        )
+        self.client.force_login(self.admin)
+
+    def test_comment_approve(self):
+        response = self.client.post(
+            reverse("atua_comment_delete", args=(self.comment.id,)),
+            HTTP_HOST=f"{self.user.username}.{settings.CANONICAL_HOST}",
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(models.Comment.objects.all().count(), 0)
