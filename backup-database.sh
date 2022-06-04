f79b613f40eccb838ed717d8e7d930ec665dfc7a
Theodore Keloglou <zf@sirodoht.com>
2022-06-04 23:42:29 +0100

    fix shellcheck lint errors on backup script

--- backup-database.sh
+++ backup-database.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-cd /home/roa/mataroa
+cd /home/roa/mataroa || exit 1
+
 source .envrc
+
 pg_dump -Fc --no-acl mataroa -h localhost -U mataroa -f /home/roa/mataroa.dump -w
-/usr/local/bin/mc cp /home/roa/mataroa.dump s3scw/tamaroa/backups/postgres-mataroa-$(date --utc +%Y-%m-%d-%H-%M-%S)/
+/usr/local/bin/mc cp /home/roa/mataroa.dump s3scw/tamaroa/backups/postgres-mataroa-"$(date --utc +%Y-%m-%d-%H-%M-%S)"/
