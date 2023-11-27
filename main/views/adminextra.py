c83978f587ecb2258130dafe237afc00480c25bc
Theodore Keloglou <zf@sirodoht.com>
2023-11-27 21:05:21 +0000

    remove approval templates

--- main/views/adminextra.py
+++ main/views/adminextra.py
@@ -55,14 +55,7 @@ def user_approve(request, user_id):
         messages.add_message(request, messages.SUCCESS, "user has been approved")
         return redirect(referer_url)
 
-    return render(
-        request,
-        "main/adminextra_user_approve.html",
-        {
-            "user": models.User.objects.get(id=user_id),
-        },
-    )
-
+    raise Http404()
 
 def user_unapprove(request, user_id):
     if not request.user.is_authenticated or not request.user.is_superuser:
@@ -77,10 +70,4 @@ def user_unapprove(request, user_id):
         messages.add_message(request, messages.SUCCESS, "user is no longer approved")
         return redirect(referer_url)
 
-    return render(
-        request,
-        "main/adminextra_user_unapprove.html",
-        {
-            "user": models.User.objects.get(id=user_id),
-        },
-    )
+    raise Http404()
